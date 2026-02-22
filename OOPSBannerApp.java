302392e8257127e9678542407b2ee2f4fbdbd9c4
thiyatira <thiyatira@gmail.com>
2026-02-22 23:33:03 +0530

    Coded UC5 to Print OOPS Banner to Console array initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,17 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
+        String[] lines = {
+            String.join(" ","  *****  ","  *****  "," ******** ","  ****** "),
+            String.join(" "," *     * "," *     * "," *      *"," *      "),
+            String.join(" ","*       *","*       *"," *      *"," *      "),
+            String.join(" ","*       *","*       *"," ********","  ***** "),
+            String.join(" ","*       *","*       *"," *       ","       *"),
+            String.join(" "," *     * "," *     * "," *       "," *     *"),
+            String.join(" ","  *****  ","  *****  "," *       ","  ***** ")
+        };
 
-        lines[0] = String.join(" ","  *****  ","  *****  "," ******** ","  ****** ");
-        lines[1] = String.join(" "," *     * "," *     * "," *      *"," *      ");
-        lines[2] = String.join(" ","*       *","*       *"," *      *"," *      ");
-        lines[3] = String.join(" ","*       *","*       *"," ********","  ***** ");
-        lines[4] = String.join(" ","*       *","*       *"," *       ","       *");
-        lines[5] = String.join(" "," *     * "," *     * "," *       "," *     *");
-        lines[6] = String.join(" ","  *****  ","  *****  "," *       ","  ***** ");
-
-        for(String line : lines) {
+        for (String line : lines) {
             System.out.println(line);
         }
     }
